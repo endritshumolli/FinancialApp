18785c0c603f5e9bcbe1a351d03e13626b65d2c8
PC <PC@DESKTOP-3N4ENGI>
2022-12-24 22:55:39 +0100

    Add project files.

---
+++ Models/Transaction.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FinancialApp.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        [Column(TypeName ="nvarchar(12)")]
+        [Required]
+        [MaxLength(11, ErrorMessage="Maximum 11 characters only.")]
+        public string AccountNumber { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        [MaxLength(11, ErrorMessage = "Maximum 11 characters only.")]
+        public string BeneficiaryName { get; set; }
+        [Column(TypeName = "nvarchar(100)")]
+        [Required]
+        [MaxLength(11, ErrorMessage = "Maximum 11 characters only.")]
+        public string BankName { get; set; }
+        [Column(TypeName = "nvarchar(11)")]
+        [Required]
+        [MaxLength(11, ErrorMessage = "Maximum 11 characters only.")]
+        public string SWIFTCode { get; set; }
+        [Required]
+        public int Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
